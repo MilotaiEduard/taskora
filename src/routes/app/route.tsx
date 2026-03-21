fca1db7db14128b38b8620324c5d0a90d2194e72
MilotaiEduard <eduard_milotai@yahoo.com>
2026-03-21 22:28:08 +0200

    Add authentication check before loading app route

--- src/routes/app/route.tsx
+++ src/routes/app/route.tsx
@@ -1,14 +1,28 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../firebase/firebase";
 
 export const Route = createFileRoute("/app")({
+  beforeLoad: async () => {
+    const user = await new Promise((resolve) => {
+      const unsubscribe = onAuthStateChanged(auth, (user) => {
+        unsubscribe();
+        resolve(user);
+      });
+    });
+
+    if (!user) {
+      throw redirect({
+        to: "/login",
+      });
+    }
+  },
   component: AppLayout,
 });
 
 function AppLayout() {
   return (
-    <div>
-      <h1>Hello app</h1>
-
+    <div className="app-layout">
       <Outlet />
     </div>
   );
